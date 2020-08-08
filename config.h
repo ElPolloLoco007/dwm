174997c4dab809d8f752e28cb1f0cf00e4968795
Helgi Poulsen <helgipoulsen@hotmail.com>
2020-08-08 17:51:21 +0100

    cool

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "ubuntuMono-R:size=12" };
+static const char *fonts[]          = { "ubuntuMono-R:size=12","JoyPixels:12" };
 static const char dmenufont[]       = "ubuntuMono-R:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -27,16 +27,24 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "🏚", "⚒", "🚩", "🌍", "🎶", "🎥", "🐔", "📝", "📬" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class             instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",            NULL,       NULL,       0,            1,           -1 },
+	{ "code",            NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Spotify",         NULL,       NULL,       1 << 4,       0,           -1 },
+	{ "Thunderbird",     NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "mpv",             NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "Steam",           NULL,       NULL,       1 << 6,       0,           -1 },
+    { "libreoffice",     NULL,       NULL,       1 << 7,       0,           -1 },
+    { "newsboat",        NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "calendar",        NULL,       NULL,       1 << 5,       0,           -1 },
+
 };
 
 /* layout(s) */
@@ -66,12 +74,18 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *termcmdNewsboat[]  = { "st","-c","newsboat","-e","newsboat", NULL };
+static const char *termcmdCalendar[]  = { "st","-c","calendar","-e","calendar", NULL };
+
+
 
 #include "shiftview.c"
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_n,      spawn,          {.v = termcmdNewsboat } },
+	{ MODKEY|ShiftMask,             XK_n,      spawn,          {.v = termcmdCalendar } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,              			XK_Right,  shiftview,      { .i = +1 } },
 	{ MODKEY,              			XK_Left,   shiftview,      { .i = -1 } },
