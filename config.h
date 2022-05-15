4841598972087a9eb6584d2127f1810b3e0910dd
Helgi Poulsen <helgipoulsen@protonmail.com>
2022-05-15 04:29:43 +0100

    layout grid

--- config.h
+++ config.h
@@ -58,11 +58,13 @@ static const int nmaster    	= 1;    /* number of clients in master area */
 static const int resizehints 	= 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+#include "layouts.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "侀",      tile },    /* first entry is default */
 	{ "",      NULL },    /* no layout function means floating behavior */
 	{ "恵",      monocle },
+	{ "﩯",      grid },
 	{ NULL,		NULL },
 };
 
