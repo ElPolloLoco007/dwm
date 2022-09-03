dbb6345d4be5d60915c0f7c6cd0e902f6041feed
Helgi Poulsen <helgipoulsen@protonmail.com>
2022-09-03 17:37:40 +0100

    swap +=

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # time keeps changing when dual booting win/linux
 timedatectl set-local-rtc 1 --adjust-system-clock &
+# swap + and =
+sudo xmodmap -e "keycode 21 = plus equal" &
+
 dwm_streamdeck &
 dwm_investment &
 dwm_news &
