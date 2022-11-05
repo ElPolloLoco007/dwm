7667b035a09b6261f53c9962d595db145df02604
Helgi Poulsen <helgipoulsen@protonmail.com>
2022-11-05 00:56:07 +0000

    updates

--- config.h
+++ config.h
@@ -1,21 +1,23 @@
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 3;        /* gaps between windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 4;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int showborder         = 1;        /* 0 means no border */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = { "3270Medium Nerd Font:size=14" };
-static const char dmenufont[]       = "3270Medium Nerd Font:size=14";
-static const char col_black[]       = "#242424";
-static const char col_yellow[]      = "#ffff7a";
-static const char col_cyan[]        = "#00d1d1";
+static const char *fonts[]          = { "3270Medium Nerd Font:size=20" };
+static const char dmenufont[]       = "3270Medium Nerd Font:size=20";
+static const char col_bg[]       = "#1E1D23";
+static const char col_fg[]      = "#7EC49D";
+static const char col_fg2[]        = "#8BB8D0";
+static const char col_border_sel[]      = "#8BB8D0";
+static const char col_border[]      = "#1E1D23";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_yellow, col_black, col_black },
-	[SchemeSel]  = { col_cyan, col_black,  col_yellow  },
+	[SchemeNorm] = { col_fg, col_bg, col_border },
+	[SchemeSel]  = { col_fg2, col_bg,  col_border_sel  },
 };
 
 
@@ -32,22 +34,21 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class             instance    			title       tags mask     switchtotag     isfloating   monitor */
-	{ "firefox",         NULL,       			NULL,       1 << 2,       1,              0,           -1 },
+	{ "qutebrowser",      NULL,       			NULL,       1 << 2,       1,              0,           -1 },
 	{ "Code",            "code",     			NULL,       1 << 1,       1,		      0,           -1 },
-	{ "DBeaver",         NULL,       			NULL,       1 << 3,       1,		      0,           -1 },
-	{ "vncviewer",       NULL,       			NULL,       1 << 4,       1,	   	 	  0,           -1 },
-	{ "Spotify", 		 "spotify",				NULL,       1 << 4,       1, 			  0,           -1 },
-	{ "mpv",             NULL,       			NULL,       1 << 4,       1,  			  0,           -1 },
-	{ "Steam",           NULL,       			NULL,       1 << 5,       1, 			  0,           -1 },
-	{ "Dwarf_Fortress",  "Dwarf_Fortress",      NULL,       1 << 5,       1, 			  0,            1 },
-	{ "dwarftherapist",  "dwarftherapist",      NULL,       1 << 5,       1, 			  0,            2 },
-    { "games",      	 "games",           	NULL,       1 << 5,       1, 			  0,           -1 },
-	{ "calendar",        NULL,       			NULL,       1 << 7,       1,  			  0,           -1 },
-	{ "news",     		 "news",  				NULL,       1 << 8,       0,  			  0,           -1 },
-	{ "radio",    	     "radio",       		NULL,       1 << 4,       1,  			  0,           -1 },
-	{ "newsboat-player", "newsboat-player",   	NULL,       1 << 4,       1,  			  0,           -1 },
-	{ "pi",       		 "pi",   				NULL,       1 << 6,       1,  			  0,           -1 },
-	{ "investment",      "investment",   		NULL,       1 << 7,       0,  			  0,           -1 },
+	{ "DBeaver",          NULL,       			NULL,       1 << 3,       1,		      0,           -1 },
+	{ "vncviewer",        NULL,       			NULL,       1 << 4,       1,	   	 	  0,           -1 },
+	{ "Spotify", 		 		 "spotify",				NULL,       1 << 4,       1, 			  0,           -1 },
+	{ "mpv",              NULL,       			NULL,       1 << 4,       1,  			  0,           -1 },
+	{ "Steam",            NULL,       			NULL,       1 << 5,       1, 			  0,           -1 },
+	{ "Dwarf_Fortress",   "Dwarf_Fortress",      NULL,       1 << 5,       1, 			  0,            1 },
+	{ "dwarftherapist",   "dwarftherapist",      NULL,       1 << 5,       1, 			  0,            1 },
+	{ "calendar",         NULL,       			NULL,       1 << 7,       1,  			  0,           -1 },
+	{ "news",     		 		"news",  				NULL,       1 << 8,       0,  			  0,           -1 },
+	{ "radio",    	     	"radio",       		NULL,       1 << 4,       1,  			  0,           -1 },
+	{ "newsboat-player", 	"newsboat-player",   	NULL,       1 << 4,       1,  			  0,           -1 },
+	{ "pi",       		 		"pi",   				NULL,       1 << 6,       1,  			  0,           -1 },
+	{ "investment",      	"investment",   		NULL,       1 << 7,       0,  			  0,           -1 },
 };
 
 /* layout(s) */
@@ -88,7 +89,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] 				= "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]			= { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_yellow, "-sb", col_black, "-sf", col_cyan, NULL };
+static const char *dmenucmd[]			= { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_bg, "-sf", col_fg2, NULL };
 static const char *dmenukillcmd[]		= { "dmenu_kill",NULL };
 static const char *termcmd[]  			= { "alacritty", NULL };
 
@@ -107,8 +108,8 @@ static const char *games[]  			= { "dmenu_games", NULL };
 static const char *togglelanguage[]  	= { "dwm_toggle_language", NULL };
 
 /* application commands */
-static const char *firefox[]  			= { "firefox", NULL };
-static const char *firefoxtab[]  		= { "firefox","--new-tab", "duckduckgo.com", NULL };
+static const char *qutebrowser[]  			= { "qutebrowser", NULL };
+static const char *qutebrowsertab[]  		= { "qutebrowser",":open -t", "duckduckgo.com", NULL };
 static const char *flameshot[]  		= { "flameshot","gui", NULL };
 static const char *slock[]  			= { "sudo","slock", NULL };
 
@@ -144,8 +145,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,    		  	XK_0,           tag,            {.ui = ~0 } },	
 	{ MODKEY,                       XK_Return,      zoom,           {0} },
 	{ MODKEY,                       XK_space,     	spawn,          {.v = togglelanguage }},
-	{ MODKEY,                       XK_f,           spawn,          {.v = firefoxtab }},
-	{ MODKEY|ShiftMask,             XK_f,           spawn,          {.v = firefox }},
+	{ MODKEY,                       XK_f,           spawn,          {.v = qutebrowsertab }},
+	{ MODKEY|ShiftMask,             XK_f,           spawn,          {.v = qutebrowser }},
 	{ MODKEY,			      		XK_m,           spawn,          {.v = media }},
 	{ MODKEY,			      		XK_g,           spawn,          {.v = games }},
 	{ MODKEY,                       XK_F3,          spawn,          {.v = screens }},
